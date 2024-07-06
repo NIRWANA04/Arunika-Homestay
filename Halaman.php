dde23fca08067754e495165bc0b8feaa951c4ec8
NIRWANA04 <127105633+NIRWANA04@users.noreply.github.com>
2024-07-06 16:17:10 +0800

    Halaman.php

--- Halaman.php
+++ Halaman.php
@@ -177,7 +177,7 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
         <div class="container">
             <a class="navbar-brand" href="#">
-                <img src="logo.png" alt="Arunika Homestay" width="120">
+                 <img src="logo.png" alt="Arunika Homestay" width="120">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
